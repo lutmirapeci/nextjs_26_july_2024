16a8491698d2f8c1b50f7941cf60ad850f3e2be1
lutmirapeci <lutmirapeci@gmail.com>
2024-09-13 14:04:49 +0200

    fixes

--- reactJs_project/movie_search_app/src/main.jsx
+++ reactJs_project/movie_search_app/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// main container
 import App from './App.jsx'
+// custom styles
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
